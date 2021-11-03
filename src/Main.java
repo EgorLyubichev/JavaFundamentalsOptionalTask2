fc92ff77b87e4b91c38462015c8c3cff683a50f1
Egor Lyubichev <EgorLyubichev@mail.ru>
2021-11-03 22:40:05 +0300

    Done II task

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,7 @@ public class Main {
         int n = scanner1.nextInt();
         int[][] mainArray = getMatrix(n);
         headAriseArray(mainArray);
+        maxQuantityIncreaseNumbers(mainArray);
         sumOfNegativeNumbersAfterTheFirstPositiveToNextPositive(mainArray);
     }
 
@@ -57,34 +58,85 @@ public class Main {
         }
         return theFirstMatrix;}
 // 2) Найти и вывести наибольшее число возрастающих (убывающих) элементов матрицы, идущих подряд.
+    public static int maxQuantityIncreaseNumbers(int[][] mainArray){
+    int[][] horizontalArray = new int[mainArray.length][mainArray.length];
+    int[][] verticalArray = new int[mainArray.length][mainArray.length];
+    for (int i = 0; i < mainArray.length; i++) { //сначала делаем единички и нолики для строк
+        System.out.print((i+1) + ")  ");
+        horizontalArray[i][0] = 0;
+        for (int j = 1; j < mainArray.length; j++) {
+            if (mainArray[i][j] > mainArray[i][j-1]){
+                horizontalArray[i][j] = horizontalArray[i][j-1] + 1;
+            }else {horizontalArray[i][j] = 0;}
+            System.out.print(horizontalArray[i][j] + " | ");
+        }   System.out.println();
+    }       System.out.println();
 
-// 3) Найти сумму элементов матрицы, расположенных между первым и вторым положительными элементами каждой строки.
-    public static int sumOfNegativeNumbersAfterTheFirstPositiveToNextPositive(int[][] mainArray){
-        int markerFirstPositiveNumber = -1;
-        int sum = 0;
-            for (int i = 0; i < mainArray.length; i++) {
-            for (int j = 0; j < mainArray[i].length; j++) {
+    for (int i = 0; i < mainArray.length; i++) {
+        System.out.print((i+1) + ")  ");
+        verticalArray[0][i] = 0;
+        for (int j = 1; j < mainArray.length; j++) {
+            if (mainArray[j][i] > mainArray[j-1][i]){
+                verticalArray[j][i] = verticalArray[j-1][i] + 1;
+            }else{verticalArray[j][i] = 0;}
+            System.out.print(verticalArray[j][i] + " | ");
+        }
+        System.out.println();
+    }
 
-            if(mainArray[i][j] > 0 && markerFirstPositiveNumber == -1){
-                markerFirstPositiveNumber = j;
-            } else if(mainArray[i][j] < 0 && markerFirstPositiveNumber == -1){
-                continue;
-            } else if(mainArray[i][j] > 0 && markerFirstPositiveNumber != -1){
-                break;
-            }else if(mainArray[i][j] < 0 && markerFirstPositiveNumber != -1){
-                sum = sum + mainArray[i][j];
+    int quentity = 0;
+    for (int i = 0; i < horizontalArray.length; i++) {
+        for (int j = 0; j < horizontalArray.length; j++) {
+            if(horizontalArray[i][j] > quentity){
+                quentity = horizontalArray[i][j];
             }
         }
-        markerFirstPositiveNumber = -1;
-
     }
-        System.out.println(sum);
+    for (int i = 0; i < verticalArray.length; i++) {
+        for (int j = 0; j < verticalArray.length; j++) {
+            if(verticalArray[i][j] > quentity){
+                quentity = verticalArray[i][j];
+            }
+        }
+    }
+    System.out.println("Наибольшее число возрастающих элементов матрицы, идущих подряд равно " + quentity);
+    return quentity;}
+// 3) Найти сумму элементов матрицы, расположенных между первым и вторым положительными элементами каждой строки.
+    public static int sumOfNegativeNumbersAfterTheFirstPositiveToNextPositive(int[][] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            int quantity = 0;
+            String firstPositiveNumber = "No";
+            String nextPositiveNumber = "No";
+            for (int j = 0; j < array[i].length; j++) {
+                if (array[i][j] >= 0 && quantity == 0 || array[i][j] >= 0 && quantity == 1) {
+                    quantity++;
+                }
+            }
+            if (quantity == 2) {
+                for (int j = 0; j < array[i].length; j++) {
+                    if (array[i][j] >= 0 && firstPositiveNumber == "No") {
+                        firstPositiveNumber = "Yes";
+                        continue;
+                    } else if (array[i][j] < 0 && firstPositiveNumber == "No") {
+                        continue;
+                    } else if (array[i][j] >= 0 && firstPositiveNumber == "Yes") {
+                        nextPositiveNumber = "Yes";
+                    } else if (array[i][j] < 0 && firstPositiveNumber == "Yes" && nextPositiveNumber == "No") {
+                        sum = sum + array[i][j];
+                    } else if (nextPositiveNumber == "Yes") {
+                        continue;
+                    }
+                }
+            }
+        }
+            System.out.println("Сумма отрицательных чисел, считая от первого положительного " +
+                    "числа до второго положительного числа, = " + sum);
             return sum;}
 // 4) Найти максимальный элемент в матрице и удалить из матрицы все строки и столбцы, его содержащие.
+// Предлагаю здесь разбить на 2 метода. 1-й ищет максимальное число и его (их) координаты.
+// 2-й вырезает из массива строки и столбцы по координатам.
 
 
+    }
 
-
-
-
-}
